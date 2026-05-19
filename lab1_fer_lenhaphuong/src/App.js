99ec353eae2410c2f552b2b96d96ec5298bf9a3c
nhaphuong22 <nhaphuong220905@gmail.com>
2026-05-19 23:00:31 +0700

    add lab1

---
+++ lab1_fer_lenhaphuong/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import './components/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
